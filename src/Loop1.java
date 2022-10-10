e435d9919c3a3ec5834d11223cc0bf1356e576c8
asus <asus@LAPTOP-IBKQ5OHU>
2022-10-10 13:26:53 +0200

    "print the number from 0 of n"

---
+++ src/Loop1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Loop1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner myInput = new Scanner(System.in);
+		System.out.println("enter the number : ");
+	   	int n = myInput.nextInt();
+		
+		
+		
+		for( int i = 1; i<=n; i++) {
+			
+			if(i< n) {
+				System.out.print(i + ",");
+			}else {
+			
+			System.out.print(i);}
+			
+			
+		}
+	}
+
+}
