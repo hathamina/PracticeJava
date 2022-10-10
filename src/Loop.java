e8b4e62abfa9974e8175b332551dde312088d1e5
asus <asus@LAPTOP-IBKQ5OHU>
2022-10-10 13:05:43 +0200

    L

--- src/Loop.java
+++ src/Loop.java
@@ -1,16 +1,25 @@
+import java.util.Scanner;
 
-public class loop {
+public class Loop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for (int i = 1; i<=10; i++) {   
+		Scanner myInput = new Scanner(System.in);
+		
+		System.out.println("Enter size of traigngle:  ");
+		
+		int n = myInput.nextInt();
+		
+		for (int i = 1; i<= n; i++) {   
 			
 			for(int j=1; j<=i; j++) {	
 				System.out.print("*");
 			}
 			System.out.println();
+			
 		
 		}
+		
 	}
 }
