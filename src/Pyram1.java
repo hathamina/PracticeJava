80342990896eb13f800359d12cd3e0ec609fe47d
asus <asus@LAPTOP-IBKQ5OHU>
2022-10-10 22:24:35 +0200

    patt1

--- src/Pyram1.java
+++ src/Pyram1.java
@@ -8,8 +8,10 @@ public class Pyram1 {
 		Scanner myInput = new Scanner(System.in);
 		
 		System.out.println("Enter size of traigngle ");
+		
+		int n = myInput.nextInt();
 
-		for(int i = 1; i<=5; i++) {
+		for(int i = 1; i<=n; i++) {
 			for(int j=1; j<=i; j++) {	
 			System.out.print(i);
 		}
