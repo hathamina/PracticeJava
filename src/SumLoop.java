d0527c3694a89328e4b628867e942eafc0d6c7e8
asus <asus@LAPTOP-IBKQ5OHU>
2022-10-10 21:53:44 +0200

    sum

---
+++ src/SumLoop.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SumLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner myInput = new Scanner(System.in);
+		System.out.println("entrer the number :");
+		int n = myInput.nextInt();
+		
+		int oldSum =0;
+		int newSum = 0;
+		int i=1;
+		while(i<=n) {
+			newSum = oldSum + i;
+			oldSum = newSum;
+			i++;
+		}
+		
+		System.out.print(newSum);
+		
+		
+	}
+
+}
